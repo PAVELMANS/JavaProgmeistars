991312f0c7dcb2497e1e6005ed8983cd8b7f63bd
Pavels Ivanovs <32202454+PAVELMANS@users.noreply.github.com>
2017-10-12 00:36:53 +0300

    Completed Caesar and working on Vigenere

--- lesson03(reaalais)/src/com/pivanovs/Main.java
+++ lesson03(reaalais)/src/com/pivanovs/Main.java
@@ -2,7 +2,45 @@ package com.pivanovs;
 
 public class Main {
 
+    public static String codeCaesar(String input) {
+        String output = "";
+        for (char iCh : input.toCharArray()) {
+            if (Character.isLetter(iCh)) {
+                int codeICh = (int) iCh;
+                if (((codeICh >= 88) && (codeICh <= 90)) || ((codeICh >= 120) && (codeICh <= 122))) {
+                    output += (char) (codeICh - 23);
+                } else {
+                    output += (char) (codeICh + 3);
+                }
+            }
+        }
+        return output;
+    }
+
+    public static String codeVigenere(String input, String keyWord) {
+        String output = "";
+        if (keyWord.length() < input.length()) {
+            String newKeyWord = keyWord;
+            for (int i = 0; i < (input.length() / keyWord.length()) + 1; i++) {
+                newKeyWord += keyWord;
+            }
+            keyWord = newKeyWord;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            int codeICh = (int) input.charAt(i);
+            int codeKeyCh = (int) keyWord.charAt(i);
+            output += (char) (((codeICh - 65) + (codeKeyCh - 65)) / 26 + 65);
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        String input = "Hello, world!";
+        System.out.println(codeCaesar("abc"));
+        input = "ABC";
+        String keyWord = "AAA";
+        System.out.println(codeVigenere(input, keyWord));
     }
 }
