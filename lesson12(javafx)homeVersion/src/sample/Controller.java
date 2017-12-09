b3b64cf632b8f24502d6f4faacbf0c7b79a7b82b
Pavels Ivanovs <32202454+PAVELMANS@users.noreply.github.com>
2017-12-10 00:54:34 +0200

    worked on the text editor application. writes in the file perfectly, despite the fact, that reading from the file is not realised

---
+++ lesson12(javafx)homeVersion/src/sample/Controller.java
@@ -0,0 +1,58 @@
+package sample;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Controller {
+    public TextField pathField;
+    public Button openFileButton;
+    public TextArea textArea;
+    public Button closeButton;
+    public Button writeButton;
+    private File file;
+    private PrintWriter pw;
+    private BufferedReader br;
+
+    public void readTextFromFile(MouseEvent mouseEvent) {
+        try {
+            String path = pathField.getText();
+            file = new File(path);
+
+            FileWriter fw = new FileWriter(file);
+            FileReader fr = new FileReader(file);
+            pw = new PrintWriter(fw);
+            br = new BufferedReader(fr);
+            List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+            while (!lines.isEmpty()) {
+                textArea.setText(lines.get(0));
+                lines.remove(0);
+            }
+        } catch (IOException e) {
+            textArea.setText("THERE IS NO SUCH FILE\n\r" +
+                    "OR THERE IS SOMETHING WRONG!");
+        }
+        writeButton.setDisable(false);
+    }
+
+    public void closeWindow(MouseEvent mouseEvent) throws IOException {
+        pw.flush();
+        pw.close();
+        br.close();
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void writeTextIntoFile(MouseEvent mouseEvent) {
+        pw.write(textArea.getText());
+    }
+}
